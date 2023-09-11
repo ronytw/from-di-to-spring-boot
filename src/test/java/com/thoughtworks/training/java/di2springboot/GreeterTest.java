3df0598e8bddea1d3b6c3e03c4c10006ade4713f
Rony Cesana <rony.cesana@thoughtworks.com>
2023-09-11 22:58:16 +0200

    Implement tests with dummy interface implementations

--- src/test/java/com/thoughtworks/training/java/di2springboot/GreeterTest.java
+++ src/test/java/com/thoughtworks/training/java/di2springboot/GreeterTest.java
@@ -2,7 +2,12 @@ package com.thoughtworks.training.java.di2springboot;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.DURATION;
 
 class GreeterTest {
     @Test
@@ -17,10 +22,30 @@ class GreeterTest {
 
     @Test
     void greetGianlucaForTheFirstTime() {
-        final GuestBook guestBook = new PropertiesGuestBook();
-        final Greeter greeter = new GreeterWithMemory(guestBook);
+        final GuestBook anEmptyGuestBook = createEmptyGuestBook();
+        final Greeter greeter = new GreeterWithMemory(anEmptyGuestBook);
 
         final String greeting = greeter.greet("Gianluca");
         assertThat(greeting).contains("Hello Gianluca");
+        assertThat(greeting).contains("you've never been here before");
+    }
+
+    @Test
+    void greetGianlucaAfterYesterdaysVisit() {
+        final LocalDateTime yesterday = LocalDateTime.now().minus(Duration.ofDays(1));
+        final GuestBook guestBookWithYesterdaysVisit = createGuestBookWithVisitOn(yesterday);
+        final Greeter greeter = new GreeterWithMemory(guestBookWithYesterdaysVisit);
+
+        final String greeting = greeter.greet("Gianluca");
+        assertThat(greeting).contains("Hello Gianluca");
+        assertThat(greeting).contains("last time you were here was " + yesterday);
+    }
+
+    private GuestBook createEmptyGuestBook() {
+        return name -> Optional.empty();
+    }
+
+    private GuestBook createGuestBookWithVisitOn(LocalDateTime yesterday) {
+        return name -> Optional.of(yesterday);
     }
 }
